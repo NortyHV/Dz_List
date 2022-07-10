fbba5986e8a232e52b4057a6dafa2540fd5c0476
Norty <nortyhv@gmail.com>
2022-07-10 15:02:18 +0300

    3

--- src/main/java/Terminal.java
+++ src/main/java/Terminal.java
@@ -4,13 +4,26 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Terminal {
-    private IOService ioService;
+    private IOService ioService = new IOService();
+
+
     private List<String> list = new ArrayList<>();
 
+    public void run() throws IOException {
+        start();
+    }
 
-
-     public void operationChoose() {
+    public void start() throws IOException {
         ioService.write("Введите цифры, которые хотите добавить (например 3,2,5,6,7)");
+        createList();
+        operationChoose();
+    }
+
+
+
+
+     public void operationChoose() throws IOException {
+
         ioService.write("Выберите операцию:");
         ioService.write("Нажмите 1, чтобы добавить элемент в список.");
         ioService.write("Наберите 2, чтобы удалить элемент из списка.");
@@ -18,13 +31,37 @@ public class Terminal {
         ioService.write("Наберите 4, чтобы  проверить, есть ли значение в списке.");
         ioService.write("Наберите 5, чтобы  заменить значение в списке.");
         ioService.write("Введите 'exit' для выхода");
-        //Integer operation = readOperation();
+        Integer operation = readOperation();
+         switch (operation) {
+             case 0:
+
+                 break;
+             case 1:
+                 addElem();
+                 operationChoose();
+                 break;
+             case 2:
+                 removeElem ();
+                 operationChoose();
+
+                 break;
+             case 3:
+                 System.out.println("Ваш список: ");
+                 print();
+                 operationChoose();
+                 break;
+             case 4:
+
+                 break;
+             default:
+         }
+     }
 
 
-    }
 
-    private void createList (String str) throws IOException {
-        str = ioService.read();
+
+    public void createList () throws IOException {
+        String str = ioService.read();
         String[] arr = str.split(",");
         list = Arrays.asList(arr);
     }
@@ -45,18 +82,32 @@ public class Terminal {
 
 
     private int toInt(String str) {
-        int strInt = Integer.parseInt(str);
-        return strInt;
+        try {
+            int temp = Integer.parseInt(str);
+        }
+        catch (NumberFormatException nfe) {
+            System.out.println("NumberFormatException: " + nfe.getMessage());
+        }
+         return Integer.parseInt(str);
+
     }
 
 
-    private void addElem (String elem) {
-        int addElem = toInt(elem);
-        list.add(String.valueOf(addElem));
+    private void addElem () throws IOException {
+        String elem = ioService.read();
+        list.add(0, elem);
+        System.out.println("ВЫ добавили в список элемент: " + elem);
     }
 
-    private void removeElem (int index) {
+    private void removeElem () throws IOException {
+        String str = ioService.read();
+        int index = toInt(str);
+        if (index >= 0 && index <= list.size()) {
         list.remove(index);
+        System.out.println("ВЫ удачно удалили  элемент из списка ");
+       } else {
+            System.out.println("Вы ввели неправильный номер элемента");
+        }
     }
 
     private  void print() {
@@ -64,7 +115,8 @@ public class Terminal {
     }
 
     private boolean contains (String str) {
-        return list.contains(str);
+         int temp = toInt(str);
+        return list.contains(temp);
 
     }
 
@@ -72,8 +124,6 @@ public class Terminal {
         list.set(index,str);
     }
 
-    public void run(){
-        operationChoose();
-    }
+
 
 }
